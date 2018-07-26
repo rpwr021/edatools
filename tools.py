b1ce13ba1f89a38477d48f62f62ab07f1c3e5068
rpwr021 <rpawar.021@gmail.com>
2018-07-25 23:59:48 -0400

    commit

---
+++ tools.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 15:59:42 2018
+
+@author: raz
+"""
+
+import pandas as pd
+import numpy as np
+
+class quickEda():
+
+    """
+    Class Doc 
+    """
+    def missingAnalysis(self, df,opt):
+        pass
+
+    def featureTypes(self, df,cat_level):
+        pass
+
+    def checkCorrelations(self, df,opt):
+        pass
+
+    def buildPlots():
+        pass    
