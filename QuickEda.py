a19aedd3c4b7fa83e22ccc9404af616e70a8cc2b
rpwr021 <rpawar.021@gmail.com>
2018-08-09 17:48:39 -0400

    lio

--- QuickEda.py
+++ QuickEda.py
@@ -5,6 +5,8 @@ Created on Fri Feb  2 15:59:42 2018
 
 @author: raz
 """
+import re
+import datetime
 
 import numpy as np
 import pandas as pd
@@ -17,7 +19,10 @@ class QuickEda:
     """
 
     def __init__(self, data):
+        assert isinstance(data, pd.DataFrame)
         self.data = data
+        self.catfeatures = []
+        self.numfeatures = []
 
     def missing_analysis(self, opts=None):
         """ find missing data for each feature"""
@@ -35,3 +40,47 @@ class QuickEda:
 
     def plot(self, kind=None):
         "yellow"
+
+    def best_features(self, xfeat, yfeat):
+        """return top n features, try regression/logistic model"""
+        pass
+
+    def isdate(self, datestring):
+        """
+        s
+        """
+        try:
+            mat = re.match('(\d{2})[/.-](\d{2})[/.-](\d{4})$', datestring)
+            if mat is not None:
+                datetime.datetime(*(map(int, mat.groups()[-1::-1])))
+                return True
+        except ValueError:
+            pass
+        return False
+
+    def get_dtype(self, xstr):
+        """# get data type with regex (is float?)
+        """
+        if re.match("^\d+?\.\d+?$", xstr) is None:
+            if xstr.isdigit():
+                return "int"
+            else:
+                return "string"
+        return "float"
+
+    def process_dtypes(self, data):
+        """read data, and assign data types accordingly
+        """
+
+        for col, x in data.iloc[1, :].iteritems():
+            t = self.get_dtype(str(x).strip())
+            if(t == "int" or t == "float"):
+                self.numfeatures.append(col)
+                if t == "int":
+                    data[col] = data[col].astype(int)
+                else:
+                    data[col] = data[col].astype(float)
+            else:
+                self.catfeatures.append(col)
+                data[col] = data[col].astype(str)
+        return data
