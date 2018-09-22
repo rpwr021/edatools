d3b8fbd5c5633aeafcf350e472eda096b2f807de
rpwr021 <rpawar.021@gmail.com>
2018-09-22 00:38:01 -0400

    added first few features

--- src/dstools/dstools.py
+++ src/dstools/dstools.py
@@ -6,7 +6,7 @@ Created on Fri Feb  2 15:59:42 2018
 @author: raz
 """
 import re
-import datetime
+from collections import defaultdict
 
 import numpy as np
 import pandas as pd
@@ -18,11 +18,9 @@ class DStools:
     Class Doc is here
     """
 
-    def __init__(self, data):
-        assert isinstance(data, pd.DataFrame)
-        self.data = data
-        self.catfeatures = []
-        self.numfeatures = []
+    def __init__(self):
+        self.features = None
+        pass
 
     def missing_analysis(self, opts=None):
         """ find missing data for each feature"""
@@ -34,9 +32,27 @@ class DStools:
         """
         pass
 
-    def check_correlations(self, opts=None):
+    def check_correlations(self, data, numfeatures, t=.90):
         """ find highly correlated feature pairs in data, threshhold could be optional """
-        pass
+        data_cor = data[numfeatures].corr()
+        # Set the threshold and add to pairs to list
+        cor_list = []
+        # Find Pairs and update cor_list
+        for i in range(len(numfeatures)-1):
+            for j in range(i+1, len(numfeatures)):
+                if (abs(data_cor.iloc[i, j]) >= t):
+                    cor_list.append([data_cor.iloc[i, j], i+1, j-1])
+
+        if cor_list:
+            # Sort by corr coef
+            
+            sorted_list = sorted(cor_list, key=lambda x: -abs(x[0]))
+            for v, i, j in sorted_list:
+                print("%s and %s = %.5f" %
+                      (numfeatures[:][i], numfeatures[:][j], v))
+            return sorted_list
+        else:
+            print("None of the features have correlation higher than ", t)
 
     def plot(self, kind=None):
         "yellow"
@@ -46,47 +62,80 @@ class DStools:
         """return top n features, try regression/logistic model"""
         pass
 
-    def isdate(self, datestring):
+    def _isdate(self, datestring):
         """
         s
         """
-        try:
-            mat = re.match('(\d{2})[/.-](\d{2})[/.-](\d{4})$', datestring)
-            if mat is not None:
-                datetime.datetime(*(map(int, mat.groups()[-1::-1])))
-                return True
-        except ValueError:
-            pass
-        return False
+        mat = re.match('(\d{1,2})[/.-](\d{1,2})[/.-](\d{2,4})$', datestring)
 
-    def get_dtype(self, xstr):
+        try:
+            if mat:
+                # xdate = datetime.datetime(*(map(int, mat.groups()[-1::-1])))
+                return True
+            else:
+                return False
+        except ValueError:
+            print("err - isdate")
+
+    def _get_dtype(self, xstr):
         """# get data type with regex (is float?)
         """
-        if re.match("^\d+?\.\d+?$", xstr) is None:
-            if xstr.isdigit():
-                return "int"
-            else:
-                if self.isdate(xstr):
-                    return "date"
-                return "string"
-        return "float"
+        if re.match("^\d+?\.\d+?$", xstr):
+            return "float"
+        elif xstr.isdigit():
+            return "int"
+        elif self._isdate(xstr):
+            return "date"
+        else:
+            return "string"
 
-    def process_dtypes(self, data):
+    def process_dtypes(self, data, tapply=False, thr=10):
         """read data, and assign data types accordingly
         """
 
-        for col, x in data.iloc[1, :].iteritems():
-            t = self.get_dtype(str(x).strip())
-            if(t == "int" or t == "float"):
-                self.numfeatures.append(col)
-                if t == "int":
-                    data[col] = data[col].astype(int)
-                else:
-                    data[col] = data[col].astype(float)
-            else:
-                self.catfeatures.append(col)
-                data[col] = data[col].astype(str)
-        return data
-    
-    def plot_learning_curve(data, clf):
+        features = defaultdict(list)
+
+        for col, val in data.iloc[3, :].iteritems():
+            val = str(val).strip()
+            val_type = self._get_dtype(val)
+
+            try:
+                if(val_type == "int" or val_type == "float"):
+
+                    features["numfeatures"].append(col)
+
+                    if tapply:
+                        if val_type == "int":
+                            data[col] = data[col].astype(np.int64)
+                        elif val_type == 'float':
+                            data[col] = data[col].astype(np.float64)
+                elif val_type == "string":
+                    unq = data[col].nunique()
+                    if unq <= thr:
+                        features["catfeatures"].append(col)
+                        if tapply:
+                            data[col] = data[col].astype('category')
+                    else:
+                        if tapply:
+                            print("feature ", col, " contains ", unq,
+                                  " unique values, converted to numeric")
+                            data[col] = data[col].astype('category')
+                            data[col] = data[col].cat.codes
+                            features["numfeatures"].append(col)
+                        else:
+                            features["catfeatures"].append(col)
+
+                elif val_type == "date":
+                    features["dtfeatures"].append(col)
+                    if tapply:
+                        data[col] = pd.to_datetime(data[col])
+
+            except ValueError as verror:
+                print(verror, "\n Data Contains N/A values in :", col)
+
+        self.features = features
+
+        return features
+
+    def plot_learning_curve(self, data, clf):
         pass
